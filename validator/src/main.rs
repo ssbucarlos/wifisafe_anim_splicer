e5a0ab1d8b387ea8e79ef5255487b21357c280ff
Carlos Aguilar <77519735+ssbucarlos@users.noreply.github.com>
2024-01-13 00:35:57 -0600

    improved code readability, account for more edge cases

--- validator/src/main.rs
+++ validator/src/main.rs
@@ -1,10 +1,12 @@
 use anyhow::Result;
 use clap::Parser;
 use ssbh_data::prelude::*;
-use ssbh_lib::prelude::*;
+use std::collections::HashMap;
+use std::iter::zip;
 use std::fs;
 use std::path::PathBuf;
 use std::time::Instant;
+use ssbh_data::anim_data::{GroupType, NodeData, TrackValues};
 
 #[derive(Parser)]
 #[command(author, version, about, long_about=None)]
@@ -20,143 +22,114 @@ enum SafetyRating {
     Unsafe(String),
     Warning(String),
 }
-fn validate_anims(reference_anim_path: &PathBuf, modified_anim_path: &PathBuf) -> SafetyRating {
-    let modified_anim = match ssbh_lib::formats::anim::Anim::from_file(&modified_anim_path) {
+
+fn get_group_by_type(
+    anim_data: &AnimData,
+    group_type: ssbh_data::anim_data::GroupType,
+) -> Option<&ssbh_data::anim_data::GroupData> {
+    for group in &anim_data.groups {
+        if group.group_type == group_type {
+            return Some(&group);
+        }
+    }
+    None
+}
+
+fn validate_anim(reference_anim_path: &PathBuf, modified_anim_path: &PathBuf) -> SafetyRating {
+    let reference_anim = match ssbh_data::anim_data::AnimData::from_file(&reference_anim_path) {
         Ok(anim) => anim,
         Err(e) => {
             return SafetyRating::Warning(format!(
-                "Can't validate modified file, it could not be opened by ssbh_lib, error=`{}`",
+                "Reference anim could not be opened by ssbh_data, error=`{}`",
                 e
             ));
         }
     };
 
-    let reference_anim = match ssbh_lib::formats::anim::Anim::from_file(&reference_anim_path) {
+    let modified_anim = match ssbh_data::anim_data::AnimData::from_file(&modified_anim_path) {
         Ok(anim) => anim,
         Err(e) => {
-            return SafetyRating::Warning(
-                format!("Can't validate modified file, it's matching reference anim could not be opened by ssbh_lib, error=`{}`", e)
-            );
+            return SafetyRating::Warning(format!(
+                "Modified anim could not be opened by ssbh_data, error=`{}`",
+                e
+            ));
         }
     };
 
-    if let Anim::V12 { .. } = &modified_anim {
-        return SafetyRating::Warning(
-            format!("Can't validate modified file, its version 1.2 and could not have been made with the wifi_safe_anim_splicer!")
-        );
-    }
-
-    if let Anim::V12 { .. } = &reference_anim {
-        return SafetyRating::Unsafe(format!(
-            "The modifed anim has a matching vanilla animation that is version V.1.2!"
-        ));
-    }
-
-    let modified_final_frame_index = match modified_anim {
-        Anim::V21 {
-            final_frame_index, ..
-        }
-        | Anim::V20 {
-            final_frame_index, ..
-        } => final_frame_index,
-        _ => {
-            panic!("Unreachable code reached")
-        }
-    };
-
-    let reference_final_frame_index = match reference_anim {
-        Anim::V21 {
-            final_frame_index, ..
-        }
-        | Anim::V20 {
-            final_frame_index, ..
-        } => final_frame_index,
-        _ => {
-            panic!("Unreachable code reached")
-        }
-    };
-
-    if modified_final_frame_index != reference_final_frame_index {
+    if reference_anim.final_frame_index != modified_anim.final_frame_index {
         return SafetyRating::Unsafe(
             format!(
-                "The modifed anim has a final_frame_index of `{modified_final_frame_index}`, while the matching vanilla anim has a final_frame_index of `{reference_final_frame_index}`"
+                "The modifed anim has a final_frame_index of `{}`, while the matching vanilla anim has a final_frame_index of `{}`",
+                modified_anim.final_frame_index,
+                reference_anim.final_frame_index
             )
         );
     }
 
-    let reference_data = match ssbh_data::anim_data::AnimData::from_file(reference_anim_path) {
-        Err(e) => {
-            return SafetyRating::Warning(
-                format!("Can't validate modified file, it's matching reference anim could not be opened by ssbh_data, error=`{}`", e)
-            );
-        }
-        Ok(data) => data,
+    let (ref_trans_group, mod_trans_group) = match (get_group_by_type(&reference_anim, GroupType::Transform), get_group_by_type(&modified_anim, GroupType::Transform)){
+        (Some(ref_group), Some(mod_group)) => {(ref_group, mod_group)},
+        (Some(_ref_group), None) => {return SafetyRating::Unsafe("The reference anim has a transform group, but the modified group has no transform group!".to_string())},
+        (None, Some(_mod_group)) => {return SafetyRating::Warning("The modified anim has transform data, but the vanilla anim had none! As long as you're 100% sure you didn't mess with any vanilla hitbox/hurtbox bones, this can still be ok.".to_string())},
+        (None, None) => {return SafetyRating::Safe}
     };
 
-    let modified_data = match ssbh_data::anim_data::AnimData::from_file(modified_anim_path) {
-        Err(e) => {
-            return SafetyRating::Warning(format!(
-                "Can't validate modified file, it couldn't be read by ssbh_data, error=`{}`",
-                e
-            ));
-        }
-        Ok(data) => data,
-    };
+    let mod_nodes_by_name: HashMap<String, &NodeData> = mod_trans_group.nodes.iter().map(|x| (x.name.clone(), x)).collect();
 
-    use std::collections::HashMap;
-    let mut modified_bone_name_to_track = HashMap::new();
-    for group in modified_data.groups {
-        if group.group_type != ssbh_data::anim_data::GroupType::Transform {
-            continue;
-        }
-        for node in group.nodes {
-            modified_bone_name_to_track.insert(node.name.clone(), node.tracks[0].clone());
-        }
-    }
+    // For the Transform group, each Node corresponds to a bone.
+    // Each bone Node will only have one Track, which is it's transform values.
+    for reference_node in &ref_trans_group.nodes {
+        let reference_values = match reference_node.tracks.get(0) {
+            None => {
+                println!("The reference anim {:?} has a Node for bone `{}` with no transform Track at all! Skipping this bone..", reference_anim_path.file_name().unwrap_or_default(), reference_node.name);
+                continue;
+             }
+             Some(track) => {
+                match &track.values{
+                    TrackValues::Transform(values) => {values},
+                    _ => {
+                        /* Some vanilla anims like
+                        fighter/kirby/motion/jackbody/c00/jackd00specialairnrandomend.nuanmb
+                        are poorly formatted like this.
+                        */
+                        println!("The reference anim `{:?}` is poorly formatted and has vis or mat data in the transform data. Skipping that track...", reference_anim_path.file_name().unwrap_or_default());
+                        continue;
+                    }
+                }
+             },
+        };
 
-    for group in reference_data.groups {
-        if group.group_type != ssbh_data::anim_data::GroupType::Transform {
-            continue;
-        }
-        for reference_node in group.nodes {
-            let modified_track = match modified_bone_name_to_track.get(&reference_node.name) {
-                Some(track) => track,
-                None => {
-                    return SafetyRating::Unsafe(format!(
-                        "The modifed anim is missing the transform track for bone {}!",
-                        reference_node.name
-                    ));
-                }
-            };
-            let reference_values = match &reference_node.tracks[0].values {
-                ssbh_data::anim_data::TrackValues::Transform(values) => values,
-                _ => {
-                    panic!()
-                }
-            };
-            let modified_values = match &modified_track.values {
-                ssbh_data::anim_data::TrackValues::Transform(values) => values,
-                _ => {
-                    panic!()
-                }
-            };
+        let modified_node = match mod_nodes_by_name.get(&reference_node.name){
+            Some(node) => {node},
+            None => {return SafetyRating::Unsafe(format!("Modified anim missing transform node `{}`", reference_node.name))}
+        };
 
-            for (index, reference_value) in reference_values.iter().enumerate() {
-                let modified_value =  match modified_values.get(index){
-                    Some(value) => {value}
-                    None => return SafetyRating::Unsafe(
-                        format!(
-                            "The modified anim has a transform track for bone {}, but its length is shorter than the reference!", reference_node.name
-                        )
+        let modified_values = match modified_node.tracks.get(0) {
+            None => {return SafetyRating::Unsafe(format!("The modified anim is missing the Transform Track for Node `{}`", modified_node.name))},
+            Some(track) => match &track.values{
+                TrackValues::Transform(values) => {values},
+                _ => {return SafetyRating::Unsafe(format!("The modified anim is poorly formatted and has vis or mat data instead of transform data for Node `{}`", modified_node.name))}
+            }
+        };
+
+        if reference_values.len() != modified_values.len(){
+            return SafetyRating::Unsafe(format!(
+                "The Node `{}` has different amount of values in the vanilla vs the modified! Vanilla=`{}`, Modified=`{}`", 
+                modified_node.name,
+                reference_values.len(),
+                modified_values.len()))
+        }
+
+        for (index, (reference_value, modified_value)) in zip(reference_values.iter(), modified_values.iter()).enumerate(){
+            if reference_value != modified_value {
+                return SafetyRating::Unsafe(
+                    format!(
+                        "The Node `{}` at frame `{}` has differing values! Vanilla=`{:?}`, Modified=`{:?}`",
+                        modified_node.name,
+                        index,
+                        reference_value,
+                        modified_value,
                     )
-                };
-
-                if reference_value != modified_value {
-                    return SafetyRating::Unsafe(format!(
-                        "The modified anim has different values than the vanilla for bone `{}`!",
-                        reference_node.name
-                    ));
-                }
+                );
             }
         }
     }
@@ -214,7 +187,7 @@ fn validate_dirs(reference_dir: &PathBuf, modified_dir: &PathBuf) -> Result<()> 
             }
         };
 
-        match validate_anims(&matching_vanilla_anim_path, &modified_anim_path) {
+        match validate_anim(&matching_vanilla_anim_path, &modified_anim_path) {
             SafetyRating::Safe => {}
             SafetyRating::Unsafe(msg) => {
                 println!(
@@ -251,7 +224,9 @@ fn main() -> Result<()> {
         .reference_folder
         .expect("Reference Folder not provided!");
 
-    let modified_dir = args.modified_folder.expect("Modified Folder not provided!");
+    let modified_dir = args
+        .modified_folder
+        .expect("Modified Folder not provided!");
 
     println!("Now validating, please wait...");
     let result = validate_dirs(&reference_dir, &modified_dir);
